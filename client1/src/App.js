a4cb24195de580e1006ef1c465eddf0c07b26f96
YuliaBondar <uliabondar359@gmail.com>
2025-01-22 19:23:37 +0300

    add in testbranch

--- client1/src/App.js
+++ client1/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+// import { Routes, Route } from 'react-router-dom';
+import Header from './widgets/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header/>
     </div>
   );
 }
---
+++ client1/src/widgets/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Logo from '../../shared/photo/logo.jpg'
+
+const Header = () => {
+  return (
+    <div className='header-component'>
+        <img src={Logo} alt="logo working work"/>
+    </div>
+  )
+}
+
+export default Header
