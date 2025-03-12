8d6361d71a1b83b4f05af3ecdf49f440bfdd5dca
YuliaBondar <uliabondar359@gmail.com>
2025-03-13 02:29:39 +0300

    add changes for creating tasks

--- client1/src/pages/CreatingTask/ui/OrderForm/OrderForm.jsx
+++ client1/src/pages/CreatingTask/ui/OrderForm/OrderForm.jsx
@@ -1,78 +1,67 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { useLocation } from 'react-router-dom';
 import './OrderForm.css';
 import Input from '../../../../shared/ui/Input/Input';
 import Button from '../../../../shared/ui/Button/Button';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
-import serviceDetails from '../../model/serviceDetails';
 import SelectInput from '../../../../shared/ui/SelectInput/SelectInput';
 import { handleCreateTask } from '../../model/tasksHandlers';
 import Cookies from 'js-cookie';
-import { toast } from 'react-toastify'
-import { useNavigate } from 'react-router-dom'
-
+import { toast } from 'react-toastify';
+import { useNavigate } from 'react-router-dom';
+import useOrderForm from '../../lib/hooks/useOrderForm';
 
 const OrderForm = () => {
-
-    const navigate = useNavigate()
+    const navigate = useNavigate();
     const location = useLocation();
 
     const queryParams = new URLSearchParams(location.search);
     const selectedService = queryParams.get('service');
-    const selectedSubcategory = queryParams.get('subcategory') || ''; 
+    const selectedSubcategory = queryParams.get('subcategory') || '';
 
-
-    const [categories, setCategories] = useState([]);
-    const [subcategories, setSubcategories] = useState([]);
+    const {
+        categories,
+        subcategories,
+        category,
+        subcategory,
+        addressFrom,
+        addressTo,
+        startDate,
+        endDate,
+        description,
+        setCategory,
+        setSubcategory,
+        setAddressFrom,
+        setAddressTo,
+        setStartDate,
+        setEndDate,
+        setDescription,
+        handleCategoryChange
+    } = useOrderForm(selectedService, selectedSubcategory);
     
-    const [category, setCategory] = useState(selectedService || ''); 
-    const [subcategory, setSubcategory] = useState(selectedSubcategory || '');
-
-    const [address, setAddress] = useState('');
-    const [startDate, setStartDate] = useState(null);
-    const [endDate, setEndDate] = useState(null);
-    const [description, setDescription] = useState('');
-
-
-    useEffect(() => {
-        setCategories(Object.keys(serviceDetails));
-        if (selectedService) {
-            setSubcategories(serviceDetails[selectedService]?.links || []);
-        }
-        if (selectedSubcategory) {
-            setSubcategory(selectedSubcategory); 
-        }
-    }, [selectedService, selectedSubcategory]);
-    
-
-    const handleCategoryChange = (e) => {
-        const selectedCategory = e.target.value;
-        setCategory(selectedCategory);
-        setSubcategories(serviceDetails[selectedCategory]?.links || []);
-        setSubcategory(''); 
-    };
-
-    const onSubmitCreateTasks = async(e) =>{
+    const onSubmitCreateTasks = async (e) => {
+        e.preventDefault(); 
         const token = Cookies.get('token');
         if (!token) {
             toast.error('Пожалуйста, войдите в систему, чтобы оформить задание.');
-            // return; 
-            setTimeout(()=>{
-                navigate('/SignIn')
-            }, 2000)
-           return
+            setTimeout(() => {
+                navigate('/SignIn');
+            }, 2000);
+            return;
         }
-        // else{
-            await handleCreateTask(e,category, subcategory,address,startDate,endDate, description)
-        // }
-       
-    }
+
+        await handleCreateTask(e, category, subcategory,addressTo, startDate, endDate, description,
+            setSubcategory, setCategory, setAddressFrom, setAddressTo, setStartDate, setEndDate, setDescription
+        );
+    };
 
     return (
         <div className='order_form_component'>
             <div className="forms_for_tasks">
-            <SelectInput
+            <span style={{ color: '#EE5300' , fontSize:'1.2rem'}}>Создавайте и описывайте задания,<br/> оформляйте и ждите ответ!</span>
+            
+                <SelectInput
                     id="category"
                     label="Категория"
                     value={category}
@@ -90,15 +79,38 @@ const OrderForm = () => {
                     required
                 />
 
-                <Input
-                    type='text'
-                    className='inputInt-field'
-                    name='address'
-                    label={'Адрес'}
-                    required
-                    value={address}
-                    onChange={(e) => setAddress(e.target.value)}
-                />
+                {category === 'Курьерские услуги' ? (
+                    <>
+                        <Input
+                            type='text'
+                            className='inputInt-field'
+                            name='addressFrom'
+                            label={'Адрес (откуда)'}
+                            required
+                            value={addressFrom}
+                            onChange={(e) => setAddressFrom(e.target.value)}
+                        />
+                        <Input
+                            type='text'
+                            className='inputInt-field'
+                            name='addressTo'
+                            label={'Адрес (куда)'}
+                            required
+                            value={addressTo}
+                            onChange={(e) => setAddressTo(e.target.value)}
+                        />
+                    </>
+                ) : (
+                    <Input
+                        type='text'
+                        className='inputInt-field'
+                        name='addressTo'
+                        label={'Адрес (куда)'}
+                        required
+                        value={addressTo}
+                        onChange={(e) => setAddressTo(e.target.value)}
+                    />
+                )}
 
                 <div className="date_fields">
                     <div className="date_field">
@@ -126,20 +138,22 @@ const OrderForm = () => {
                     <textarea
                         name="taskDescription"
                         className="textarea-field"
-                        placeholder="Опишите детали вашего задания..."
+                        placeholder="Опишите детали задания..."
                         value={description}
                         onChange={(e) => setDescription(e.target.value)}
+                        required
+
                     />
                 </div>
 
                 <Button
-                onClick={onSubmitCreateTasks}
+                    onClick={onSubmitCreateTasks}
                     text='Оформить задание'
                     style={{ backgroundColor: '#EE5300', color: 'black', border: '2px solid #EE5300', width: '100%', height: '6vh', fontWeight: 'bold' }}
                 />
             </div>
         </div>
     );
-}
+};
 
 export default OrderForm;
--- client1/src/pages/FindTask/FindTask.jsx
+++ client1/src/pages/FindTask/FindTask.jsx
@@ -1,24 +1,24 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import './FindTask.css';
 import InputService from '../../shared/ui/InputService/InputService';
-import AuthPeople from '../../api/userApi'; 
+// import AuthPeople from '../../api/userApi'; 
 
 const FindTask = () => {
-  const [users, setUsers] = useState([]);
-  const [error, setError] = useState(null); 
+  // const [users, setUsers] = useState([]);
+  // const [error, setError] = useState(null); 
 
-  useEffect(() => {
-    const fetchUsers = async () => {
-      try {
-        const usersData = await AuthPeople.getUsers();
-        setUsers(usersData); // Убедитесь, что данные правильно устанавливаются
-      } catch (error) {
-        setError(error); 
-      }
-    };
+  // useEffect(() => {
+  //   const fetchUsers = async () => {
+  //     try {
+  //       const usersData = await AuthPeople.getUsers();
+  //       setUsers(usersData); // Убедитесь, что данные правильно устанавливаются
+  //     } catch (error) {
+  //       setError(error); 
+  //     }
+  //   };
 
-    fetchUsers();
-  }, []);
+  //   fetchUsers();
+  // }, []);
 
   return (
     <div className='findTask_component'>
@@ -28,7 +28,7 @@ const FindTask = () => {
           <InputService placeholder="Например, требуется курьер, доставить товар" />
         </div>
       </div>
-      <div className="all_users">
+      {/* <div className="all_users">
         {error ? (
           <p>Ошибка при загрузке данных: {error.message}</p>
         ) : users.length > 0 ? (
@@ -42,7 +42,7 @@ const FindTask = () => {
         ) : (
           <p>Пользователи не найдены.</p>
         )}
-      </div>
+      </div> */}
     </div>
   );
 };
