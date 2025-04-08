9e0c51b5160b44dc8d4865913786a790ba549148
Artsiom <masloartem20@gmail.com>
2025-04-08 21:50:08 +0300

    delete headers from requests

--- client1/src/api/UserInfoApi.js
+++ client1/src/api/UserInfoApi.js
@@ -1,104 +1,125 @@
-import axios from 'axios'
-import {baseURL} from '../constants/someConstants'
+import axios from 'axios';
+import { baseURL } from '../constants/someConstants';
 import Cookies from 'js-cookie';
 
 class UserInfo {
-    
-    async changePassword({ OldPassword, Password, NewPassword }) {
-        try {
-            const response = await axios.post(`${baseURL}/user-info/change-password`, {
-                OldPassword, Password, NewPassword
-            });
-            return response.data;
-        } catch (error) {
-            console.error('Ошибка при регистрации:', error.response?.data || error.message);
-            throw error;
+  async changePassword({ OldPassword, Password, NewPassword }) {
+    try {
+      const response = await axios.post(
+        `${baseURL}/user-info/change-password`,
+        {
+          OldPassword,
+          Password,
+          NewPassword,
         }
+      );
+      return response.data;
+    } catch (error) {
+      console.error(
+        'Ошибка при регистрации:',
+        error.response?.data || error.message
+      );
+      throw error;
     }
+  }
 
-    async getUserInfo (){
-        try{
-            const response = await axios.get(`${baseURL}/user-info/get-info`);
-            return response.data;
-
-        }catch(error){
-            console.log('Ошибка получения данных пользователя', error.response);
-            throw error;
-        }
+  async getUserInfo() {
+    try {
+      const response = await axios.get(`${baseURL}/user-info/get-info`);
+      return response.data;
+    } catch (error) {
+      console.log('Ошибка получения данных пользователя', error.response);
+      throw error;
     }
+  }
 
-    async addMobilePhone({ PhoneNumber }) {
-        try {
-            const access_token = Cookies.get('access_token');
-            if (!access_token) throw new Error('Не найден токен');
-            
-            const response = await axios.post(`${baseURL}/user-info/add-phone-number`, 
-                { 
-                    PhoneNumber 
-                },
-                { 
-                    headers: { Authorization: `Bearer ${access_token}` },
-                    withCredentials: true 
-                }
-            );
-            console.log('ответ', response.data);  // Выводим ответ сервера
-           
-            return response.data;
-        } catch (error) {
-            console.error('Ошибка добавления номера телефона:', error.response?.data || error.message);
-            throw error; // Бросаем ошибку дальше
-        }
-    }
+  async addMobilePhone({ PhoneNumber }) {
+    try {
+      const access_token = Cookies.get('access_token');
+      if (!access_token) throw new Error('Не найден токен');
 
+      const response = await axios.post(
+        `${baseURL}/user-info/add-phone-number`,
+        {
+          PhoneNumber,
+        },
+        {
+          withCredentials: true,
+        }
+      );
+      console.log('ответ', response.data); // Выводим ответ сервера
 
-    async addUserInfo({ Name, Surname, BirthdayDate, Sex, City, Email }) {
-        
-        try {
-           
-            const access_token = Cookies.get('access_token');
-            console.log('Токен:', access_token); // Должен быть валидный JWT
-            if (!access_token) {
-            console.error('Токен не найден в куках');
-        }
-            const formattedDate = BirthdayDate ? new Date(BirthdayDate).toISOString() : null;
-            console.log("Отправляемые данные:", { 
-                Name, Surname, BirthdayDate: formattedDate, Sex, City, Email 
-            });
-    
-            const response = await axios.post(`${baseURL}/user-info/add-user-info`, 
-                { Name, Surname, BirthdayDate: formattedDate, Sex, City, Email },
-                { 
-                    headers: { Authorization: `Bearer ${access_token}` },
-                    withCredentials: true
-                }
-            );
-    
-            return response.data;
-        } catch (error) {
-            console.error('Ошибка сохранения данных:', error.response?.data || error.message);
-            throw error;
-        }
+      return response.data;
+    } catch (error) {
+      console.error(
+        'Ошибка добавления номера телефона:',
+        error.response?.data || error.message
+      );
+      throw error; // Бросаем ошибку дальше
     }
-    
-    
-    async uploadFilePhoto(file) {
-        const formData = new FormData(); 
-        formData.append('file', file); 
-    
-        try {
-            const response = await axios.post(`${baseURL}/user-info/upload-avatar`, formData, {
-                headers: {
-                    'Content-Type': 'multipart/form-data', 
-                },
-            });
-            return response.data; 
-        } catch (error) {
-            console.log('Ошибка добавления информации о пользователе', error.response);
-            return null; 
+  }
+
+  async addUserInfo({ Name, Surname, BirthdayDate, Sex, City, Email }) {
+    try {
+      const access_token = Cookies.get('access_token');
+      console.log('Токен:', access_token); // Должен быть валидный JWT
+      if (!access_token) {
+        console.error('Токен не найден в куках');
+      }
+      const formattedDate = BirthdayDate
+        ? new Date(BirthdayDate).toISOString()
+        : null;
+      console.log('Отправляемые данные:', {
+        Name,
+        Surname,
+        BirthdayDate: formattedDate,
+        Sex,
+        City,
+        Email,
+      });
+
+      const response = await axios.post(
+        `${baseURL}/user-info/add-user-info`,
+        { Name, Surname, BirthdayDate: formattedDate, Sex, City, Email },
+        {
+          withCredentials: true,
         }
+      );
+
+      return response.data;
+    } catch (error) {
+      console.error(
+        'Ошибка сохранения данных:',
+        error.response?.data || error.message
+      );
+      throw error;
     }
+  }
+
+  async uploadFilePhoto(file) {
+    const formData = new FormData();
+    formData.append('file', file);
+
+    try {
+      const response = await axios.post(
+        `${baseURL}/user-info/upload-avatar`,
+        formData,
+        {
+          headers: {
+            'Content-Type': 'multipart/form-data',
+          },
+        }
+      );
+      return response.data;
+    } catch (error) {
+      console.log(
+        'Ошибка добавления информации о пользователе',
+        error.response
+      );
+      return null;
+    }
+  }
 }
-
 
 const UsInfo = new UserInfo();
 export default UsInfo;
--- client1/src/api/userApi.js
+++ client1/src/api/userApi.js
@@ -1,96 +1,114 @@
-import axios from 'axios'
-import {baseURL} from '../constants/someConstants'
+import axios from 'axios';
+import { baseURL } from '../constants/someConstants';
 import Cookies from 'js-cookie';
 // import { toast } from 'react-toastify';
 
 class Auth {
-    
-    async registerUser({ UserName, Email, Password }) {
-        try {
-            const response = await axios.post(`${baseURL}/auth/create-user`, {
-                UserName, Email,Password
-            });
-            return response.data;
-        } catch (error) {
-            // console.error('Ошибка при регистрации:', error.response?.data || error.message);
-            throw error;
-        }
+  async registerUser({ UserName, Email, Password }) {
+    try {
+      const response = await axios.post(`${baseURL}/auth/create-user`, {
+        UserName,
+        Email,
+        Password,
+      });
+      return response.data;
+    } catch (error) {
+      // console.error('Ошибка при регистрации:', error.response?.data || error.message);
+      throw error;
     }
+  }
 
-    async login({Email, Password}){
-        try{
-            const response = await axios.post(`${baseURL}/auth/login`,{
-                Email, Password
-            });
-            return response.data;
-        }catch(error){
-            console.error('Ошибка при регистрации:', error.response?.data || error.message);
-            throw error;
-        }
+  async login({ Email, Password }) {
+    try {
+      const response = await axios.post(`${baseURL}/auth/login`, {
+        Email,
+        Password,
+      });
+      return response.data;
+    } catch (error) {
+      console.error(
+        'Ошибка при регистрации:',
+        error.response?.data || error.message
+      );
+      throw error;
     }
+  }
 
-    async getUsers(){
-        try{
-            const response = await axios.get(`${baseURL}/auth/get-users`);
-            return response.data;
-        }catch(error){
-            console.error('Ошибка при регистрации:', error.response?.data || error.message);
-            throw error;
-        }
-    }    
-
-    async sendVerificationEmail(Email) {
-        try {
-            const response = await axios.post(`${baseURL}/auth/send`, { Email });
-            return response.data;
-           
-        } catch (error) {
-            console.error('Ошибка при отправке письма:', error.response?.data || error.message);
-            throw error;
-        }
+  async getUsers() {
+    try {
+      const response = await axios.get(`${baseURL}/auth/get-users`);
+      return response.data;
+    } catch (error) {
+      console.error(
+        'Ошибка при регистрации:',
+        error.response?.data || error.message
+      );
+      throw error;
     }
+  }
 
-    async verificateUserWithCodeFromEmail(Code, Email){
-        try{
-            const response = await axios.post(`${baseURL}/auth/forgotPassword`, { Code,Email });
-            return response.data;
-        }catch (error) {
-            console.log('Ошибка при отправке кода и почты')
-            throw error;
-        }
+  async sendVerificationEmail(Email) {
+    try {
+      const response = await axios.post(`${baseURL}/auth/send`, { Email });
+      return response.data;
+    } catch (error) {
+      console.error(
+        'Ошибка при отправке письма:',
+        error.response?.data || error.message
+      );
+      throw error;
     }
+  }
 
-    async resetPassword(Email, Password){
-        try{
-            const response = await axios.post(`${baseURL}/auth/resetPassword`, {Email, Password});
-            return response.data;
-        }catch(error){
-            console.log('Ошибка при изменении пароля')
-            throw error;
-        }
+  async verificateUserWithCodeFromEmail(Code, Email) {
+    try {
+      const response = await axios.post(`${baseURL}/auth/forgotPassword`, {
+        Code,
+        Email,
+      });
+      return response.data;
+    } catch (error) {
+      console.log('Ошибка при отправке кода и почты');
+      throw error;
     }
+  }
 
-    async deleteUser() {
-       
-        try {
-            const access_token = Cookies.get('access_token'); 
-            console.log('токен',access_token)
-            if (!access_token) {
-                throw new Error('Токен не найден.');
-            }
-    
-            console.log('Отправка запроса на удаление пользователя');
-    
-            const response = await axios.delete(`${baseURL}/auth/delete-users`, {
-                headers: { Authorization: `Bearer ${access_token}` },
-            });
-    
-            return response; 
-        } catch (error) {
-            console.error('Ошибка удаления пользователя:', error.response?.data || error.message);
-            throw error; 
-        }
+  async resetPassword(Email, Password) {
+    try {
+      const response = await axios.post(`${baseURL}/auth/resetPassword`, {
+        Email,
+        Password,
+      });
+      return response.data;
+    } catch (error) {
+      console.log('Ошибка при изменении пароля');
+      throw error;
     }
+  }
+
+  async deleteUser() {
+    try {
+      const access_token = Cookies.get('access_token');
+      console.log('токен', access_token);
+      if (!access_token) {
+        throw new Error('Токен не найден.');
+      }
+
+      console.log('Отправка запроса на удаление пользователя');
+
+      const response = await axios.delete(`${baseURL}/auth/delete-users`, {
+        withCredentials: true,
+      });
+
+      return response;
+    } catch (error) {
+      console.error(
+        'Ошибка удаления пользователя:',
+        error.response?.data || error.message
+      );
+      throw error;
+    }
+  }
 }
 
 const AuthPeople = new Auth();
