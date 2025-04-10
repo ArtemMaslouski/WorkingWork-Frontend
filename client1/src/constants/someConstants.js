2346f1f89da32fda3164985936918bad0dcc67a1
YuliaBondar <uliabondar359@gmail.com>
2025-04-10 22:37:30 +0300

    .

--- client1/src/constants/someConstants.js
+++ client1/src/constants/someConstants.js
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-// export const baseURL = 'https://workingworld-backend-production.up.railway.app';
-export const baseURL = 'http://localhost:3000';
-=======
 export const baseURL = 'https://workingworld-backend-production.up.railway.app';
 //export const baseURL = 'http://localhost:3000';
->>>>>>> 2ab8117f692d8876f91ed007a3bf44b702130d9b
+
