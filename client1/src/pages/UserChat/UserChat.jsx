c923bc5cabd5bcbb78464fffc1d80f6b85497339
YuliaBondar <uliabondar359@gmail.com>
2025-05-21 00:53:44 +0300

    add feature for find tasks and style for chat

--- client1/src/pages/UserChat/UserChat.jsx
+++ client1/src/pages/UserChat/UserChat.jsx
@@ -1,11 +1,35 @@
 import React from 'react'
 import './UserChat.css'
+import { MdOutlineMenu } from "react-icons/md";
+import { TfiWrite } from "react-icons/tfi";
+import { IoIosArchive } from "react-icons/io";
+// import InputService from '../../shared/ui/InputService/InputService';
 
 const UserChat = () => {
   return (
     <div className='userChat_component'>
       <div className="userChat">
-      
+        <div className="menu_chat">
+          <div className="menu_and_name">
+            <MdOutlineMenu size={30}/>
+            Чаты
+          </div>
+          <div className="menu_emblems">
+            <TfiWrite size={30}/>
+            <IoIosArchive size={30}/>
+          </div>
+         
+        </div>
+        {/* <InputService
+            placeholder="Поиск"
+            onFilterClick={() => setIsFilterOpen(true)}
+            searchQuery={searchQuery}
+            onSearchChange={setSearchQuery}
+            onSearchClick={handleSearchClick}
+          /> */}
+        <div className="chats">
+
+        </div>
       </div>
     </div>
   )
