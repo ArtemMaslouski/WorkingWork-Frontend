96b98c3efe65df31d047d046fb4baf4dd2835900
Artsiom <masloartem20@gmail.com>
2025-07-12 00:18:40 +0300

    feat: add getting all chats of users

---
+++ client1/src/api/ChatApi.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+class Chat {
+  async getChats() {
+    try {
+      const response = await axios.get(
+        `${process.env.REACT_APP_URL}/chats/get-chats`
+      );
+
+      return response.data;
+    } catch (error) {
+      console.log('nlvnsdlkv');
+      console.log(process.env.REACT_APP_URL);
+      console.error(
+        'Ошибка при получении чатов: ',
+        error.response?.data || error.message
+      );
+      throw error;
+    }
+  }
+}
+
+const ChatEntity = new Chat();
+export default ChatEntity;
--- client1/src/widgets/Header/NavLinks.jsx
+++ client1/src/widgets/Header/NavLinks.jsx
@@ -1,33 +1,39 @@
-import React, {useState, useRef, useEffect} from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { NavLink, useLocation } from 'react-router-dom';
-import { FiMapPin } from "react-icons/fi";
-import { TfiWorld } from "react-icons/tfi";
-import { FaQuestion } from "react-icons/fa";
-import { CgProfile } from "react-icons/cg";
-import { BsChatLeft } from "react-icons/bs";
+import { FiMapPin } from 'react-icons/fi';
+import { TfiWorld } from 'react-icons/tfi';
+import { FaQuestion } from 'react-icons/fa';
+import { CgProfile } from 'react-icons/cg';
+import { BsChatLeft } from 'react-icons/bs';
 import LanguageModal from '../modals/LanguageModal/LanguageModal';
 import i18n from 'i18next';
 import { useTranslation } from 'react-i18next';
+import ChatEntity from '../../api/ChatApi';
 
-const NavLinks = ({ isAuthenticated, toggleMapModal, isMapModalOpen, handleLogout, navigate }) => {
+const NavLinks = ({
+  isAuthenticated,
+  toggleMapModal,
+  isMapModalOpen,
+  handleLogout,
+  navigate,
+}) => {
   const location = useLocation();
   const [showLanguageDropdown, setShowLanguageDropdown] = useState(false);
   const langModalRef = useRef(null);
-  const {t} = useTranslation();
+  const { t } = useTranslation();
 
   const handleSelectLanguage = (lang) => {
     const langCode = {
-      'Русский': 'ru',
-      'English': 'en',
+      Русский: 'ru',
+      English: 'en',
     }[lang];
-  
+
     if (langCode) {
       i18n.changeLanguage(langCode);
-      localStorage.setItem('language', langCode); 
+      localStorage.setItem('language', langCode);
       setShowLanguageDropdown(false);
     }
   };
-
 
   const handleClickOutside = (e) => {
     if (langModalRef.current && !langModalRef.current.contains(e.target)) {
@@ -46,40 +52,80 @@ const NavLinks = ({ isAuthenticated, toggleMapModal, isMapModalOpen, handleLogou
     };
   }, [showLanguageDropdown]);
 
-
   return (
     <>
-       <NavLink to='/CreatingTask' className={({ isActive }) => 
-          (isActive || location.pathname === '/OrderForm') ? 'createExercise active' : 'createExercise'}>
-          {t('createTask')}
-        </NavLink>
-        <NavLink to='/FindTask' className={({ isActive }) => isActive ? 'findExercise active' : 'findExercise'}>{t('findTask')}</NavLink>
-      
-      <NavLink 
-        onClick={toggleMapModal} style={{ color: isMapModalOpen ? '#a5811e' : 'black' }}>
+      <NavLink
+        to='/CreatingTask'
+        className={({ isActive }) =>
+          isActive || location.pathname === '/OrderForm'
+            ? 'createExercise active'
+            : 'createExercise'
+        }
+      >
+        {t('createTask')}
+      </NavLink>
+      <NavLink
+        to='/FindTask'
+        className={({ isActive }) =>
+          isActive ? 'findExercise active' : 'findExercise'
+        }
+      >
+        {t('findTask')}
+      </NavLink>
+
+      <NavLink
+        onClick={toggleMapModal}
+        style={{ color: isMapModalOpen ? '#a5811e' : 'black' }}
+      >
         <FiMapPin size={20} />
       </NavLink>
 
-      <NavLink onClick={()=> setShowLanguageDropdown((prev) => !prev)} style={{ color: showLanguageDropdown ? '#a5811e' : 'black' }}>
+      <NavLink
+        onClick={() => setShowLanguageDropdown((prev) => !prev)}
+        style={{ color: showLanguageDropdown ? '#a5811e' : 'black' }}
+      >
         <TfiWorld size={20} />
       </NavLink>
       <div ref={langModalRef}>
-        <LanguageModal 
+        <LanguageModal
           isOpen={showLanguageDropdown}
           onSelectLanguage={handleSelectLanguage}
         />
       </div>
-      <NavLink to='/QuestionComponent' className={({ isActive }) => isActive ? 'active' : ''}><FaQuestion size={20}/></NavLink>
-    
+      <NavLink
+        to='/QuestionComponent'
+        className={({ isActive }) => (isActive ? 'active' : '')}
+      >
+        <FaQuestion size={20} />
+      </NavLink>
+
       {isAuthenticated ? (
         <>
-         <NavLink to='/UserChat' className={({ isActive }) => isActive ? 'active' : ''}><BsChatLeft size={20}/> </NavLink>
-          <NavLink to='/Profile'><CgProfile size={20}/></NavLink>
-          
-          <span onClick={() => handleLogout(navigate, t)} className='logout-link'>{t('logout')}</span>
+          <NavLink
+            onClick={() => ChatEntity.getChats()}
+            to='/UserChat'
+            className={({ isActive }) => (isActive ? 'active' : '')}
+          >
+            <BsChatLeft size={20} />{' '}
+          </NavLink>
+          <NavLink to='/Profile'>
+            <CgProfile size={20} />
+          </NavLink>
+
+          <span
+            onClick={() => handleLogout(navigate, t)}
+            className='logout-link'
+          >
+            {t('logout')}
+          </span>
         </>
       ) : (
-        <NavLink to='/SignIn' className={({ isActive }) => isActive ? 'active' : ''}>{t('login')}</NavLink>
+        <NavLink
+          to='/SignIn'
+          className={({ isActive }) => (isActive ? 'active' : '')}
+        >
+          {t('login')}
+        </NavLink>
       )}
     </>
   );
